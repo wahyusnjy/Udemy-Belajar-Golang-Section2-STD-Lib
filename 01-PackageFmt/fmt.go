b26f42e690273d62b2ee15e3296a2a0d024cbe1f
wahyusnjy <wahyusnjy@gmail.com>
2025-07-27 14:08:28 +0700

    First commit

---
+++ 01-PackageFmt/fmt.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Wahyu"
+	lastName := "Sanjaya"
+
+	fmt.Println("Hello, my name is", firstName, lastName)
+	fmt.Printf("Hello, my name is %s %s\n", firstName, lastName)
+}
